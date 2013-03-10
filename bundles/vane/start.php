2fbcc5c5e4349d23deeee1590de284c0b054ec92
Proger <proger.xp@gmail.com>
2013-03-10 21:09:11 +0400

    Added '!' (not having) features to vane::auth.

--- bundles/vane/start.php
+++ bundles/vane/start.php
@@ -41,7 +41,11 @@ overrideHTMLki('vane::', __NAMESPACE__);
 
 \Autoloader::namespaces(array(__NAMESPACE__ => __DIR__.DS.'classes'));
 
-// vane::auth[:perm[:perm.2[...]]] [|filter:2 [|...]]
+// vane::auth[:[!]perm[:[!]perm.2[...]]] [|filter:2 [|...]]
+//
+// This filter will always deny access for non-authorized users even if guest
+// permissions allow for given features - this is so because protected controllers
+// rely on current user being logged in.
 \Route::filter('vane::auth', function ($feature_1 = null) {
   $features = is_array($feature_1) ? $feature_1 : func_get_args();
   $block = is_object(end($features)) ? array_pop($features) : null;
@@ -54,10 +58,21 @@ overrideHTMLki('vane::', __NAMESPACE__);
     $deny = Log::error_Auth($msg);
   } elseif (!$user) {
     $deny = Log::info_Auth("Block needs authorized user, denying access for guest.");
-  } elseif ($missing = array_omit($features, array($user, 'can'))) {
-    $s = count($missing) == 1 ? '' : 's';
-    $deny = Log::info_Auth("Denied access via vane::auth for user {$user->id} due".
-                           " to missing permission$s; ".join(', ', $missing).'.');
+  } elseif ($features) {
+    list($toMiss, $toHave) = S::divide($features, '?[0] === "!"');
+
+    $having = array_filter(S($toMiss, array('.substr', 1)), array($user, 'can'));
+    $missing = array_omit($toHave, array($user, 'can'));
+
+    $reasons = array();
+    $having and $reasons[] = "present flag(s): ".join(', ', $having);
+    $missing and $reasons[] = "missing permission(s): ".join(', ', $missing);
+
+    if ($reasons) {
+      $msg = "Denied access via vane::auth for user {$user->id} due to ".
+             join(' and ', $reasons).'.';
+      $deny = Log::info_Auth($msg);
+    }
   }
 
   if (!empty($deny)) {
