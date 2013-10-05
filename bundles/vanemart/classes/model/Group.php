0d7af0569ba51eab27e26411ed439e93169d8cfb
Monogatari <monogatari42@gmail.com>
2013-10-05 17:07:09 +0600

    tree support

--- bundles/vanemart/classes/model/Group.php
+++ bundles/vanemart/classes/model/Group.php
@@ -14,6 +14,98 @@ class Group extends BaseModel {
     return $attrs;
   }
 
+  static function goodsCount() {
+    $table = static::$table;
+    $goodsTable = Product::$table;
+   
+    $counts = \DB::table($table)->join($goodsTable, $goodsTable.'.group', '=', $table.'.id')
+      ->group_by($table.'.id')
+      ->get(array($table.'.id', \DB::raw('COUNT('.$goodsTable.'.id) as count')));
+
+    $result = array();
+    foreach($counts as $count) {
+      $result[$count->id] = $count->count;
+    }
+
+    return $result;
+  }
+
+  static function treeWithCounts($groups = null) {
+    $counts = static::goodsCount();
+    if ($groups === null) {
+      $groups = static::get();
+    }
+    $tree = static::buildTree($groups);
+    static::calcGoodsCount($tree, $counts);
+
+    return $tree;
+  }
+
+  static function multipleChildrenIds($tree, $gids) {
+    $result = array();
+    foreach ($gids as $gid) {
+      $childs = static::findGroupInTree($tree, $gid)->childs;
+      $result = array_merge($result, static::findChildrenIds($childs));
+    }
+    return $result;
+  }
+
+  static function findChildrenIds($childs) {
+    $gids = array();
+    if ($childs) {
+      foreach ($childs as $group) {
+        $gids[] = $group->id;
+        $gids = array_merge($gids, static::findChildrenIds($group->childs));
+      }
+    }
+    return $gids;
+  }
+
+  static function findGroupInTree($tree, $gid) {
+    foreach ($tree as $group) {
+      if ($group->id == $gid) {
+        return $group;
+      }
+      if ($group->childs) {
+        $result = static::findGroupInTree($group->childs, $gid);
+        if ($result) {
+          return $result;
+        }
+      }
+    }
+    return null;
+  }
+
+  static function calcGoodsCount($tree, $counts) {
+    $sum = 0;
+    foreach ($tree as $group) {
+      $group->goodsCount = isset($counts[$group->id]) ? $counts[$group->id] : 0;
+      if ($group->childs) {
+        $group->goodsCount += static::calcGoodsCount($group->childs, $counts);
+      }
+      $sum += $group->goodsCount;
+    }
+    return $sum;
+  }
+
+  static function buildTree($items, $root = null) {
+    $childs = array();
+
+    foreach ($items as $item) {
+      $childs[$item->parent][] = $item;
+    }
+
+    foreach ($items as $item) {
+      if (isset($childs[$item->id])) {
+        $item->childs = $childs[$item->id];
+      }
+    }
+
+    $tree = $childs[$root];
+
+    return $tree;
+  }
+
   function pretty() {
     return $this->fill_raw( static::prettyOther($this->attributes) );
   }
